411b5d24d568d68695acdbf1d64dfa8363ee118d
unknown <ashwin.k.sri@gmail.com>
2018-04-29 10:02:48 +0530

    Set DEBUG = False

--- stuplan/settings.py
+++ stuplan/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+u=d9w8tbmectl$kb%vnchh^6+^!2v4i^8l_yj52@u@83!i3s9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["studyhours.herokuapp.com", "127.0.0.1"]
 
